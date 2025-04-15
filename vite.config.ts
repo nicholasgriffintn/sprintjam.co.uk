e6d70c1c1d68f0da9afa0d49e75db9d3b990efb7
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-04-15 23:52:48 +0100

    chore: get vite loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  environments: {
-    my_worker: {
-      define: {
-        __APP_VERSION__: JSON.stringify("v1.0.0"),
-      },
-    },
-  },
-  plugins: [cloudflare()],
+  plugins: [react(), tailwindcss(), cloudflare()],
 });
