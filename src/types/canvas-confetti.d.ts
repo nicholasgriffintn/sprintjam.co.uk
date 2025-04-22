5ddefa07de71066b9daaa36f836e3de1c566a05e
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-04-22 20:13:21 +0100

    fix: ts error

---
+++ src/types/canvas-confetti.d.ts
@@ -0,0 +1,12 @@
+declare module 'canvas-confetti' {
+  interface ConfettiOptions {
+    particleCount?: number;
+    spread?: number;
+    origin?: { x?: number; y?: number };
+    [key: string]: any;
+  }
+
+  function confetti(options?: ConfettiOptions): void;
+
+  export default confetti;
+} 
