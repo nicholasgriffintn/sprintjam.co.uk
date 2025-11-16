7dcdc4a44cf14bc9ac7cf77c0ed511e7c9cb25c0
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-11-16 23:57:40 +0000

    chore: update config

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,12 @@
+export const ENV_BASE_URL = import.meta.env.VITE_API_URL || 'sprintjam.co.uk';
+
 export const API_BASE_URL = import.meta.env.DEV
   ? 'http://localhost:5173/api'
-  : 'https://sprintjam.co.uk/api';
+  : `https://${ENV_BASE_URL}/api`;
 
 export const WS_BASE_URL = import.meta.env.DEV
   ? 'ws://localhost:5173/ws'
-  : 'wss://sprintjam.co.uk/ws';
+  : `wss://${ENV_BASE_URL}/ws`;
 
 export const QUICK_EMOJIS = [
   '😀',
