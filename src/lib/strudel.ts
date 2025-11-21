b9a2df6aa3170303317a006f16b8625ec7f874a2
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-11-21 21:32:12 +0000

    fix: correctly define URLs

--- src/lib/strudel.ts
+++ src/lib/strudel.ts
@@ -6,23 +6,26 @@ import {
   // @ts-expect-error - @strudel/webstudio has no type definitions
 } from "@strudel/web";
 
+const DOUGH_SAMPLES_BASE =
+  "https://raw.githubusercontent.com/felixroos/dough-samples/main";
+const TODEPOND_SAMPLES_BASE =
+  "https://raw.githubusercontent.com/todepond/samples/main";
+const DIRT_SAMPLES_BASE =
+  "https://raw.githubusercontent.com/tidalcycles/Dirt-Samples/master";
+
 async function prebake() {
-  const ds = "https://raw.githubusercontent.com/felixroos/dough-samples/main/";
-  const ts = "https://raw.githubusercontent.com/todepond/samples/main/";
-  const drts =
-    "https://raw.githubusercontent.com/tidalcycles/Dirt-Samples/master/";
   await Promise.all([
     registerSynthSounds(),
     registerZZFXSounds(),
-    samples(`${ds}/tidal-drum-machines.json`),
-    samples(`${ds}/piano.json`),
-    samples(`${ds}/Dirt-Samples.json`),
-    samples(`${ds}/EmuSP12.json`),
-    samples(`${ds}/vcsl.json`),
-    samples(`${ds}/mridangam.json`),
-    samples(`${drts}/strudel.json`),
+    samples(`${DOUGH_SAMPLES_BASE}/tidal-drum-machines.json`),
+    samples(`${DOUGH_SAMPLES_BASE}/piano.json`),
+    samples(`${DOUGH_SAMPLES_BASE}/Dirt-Samples.json`),
+    samples(`${DOUGH_SAMPLES_BASE}/EmuSP12.json`),
+    samples(`${DOUGH_SAMPLES_BASE}/vcsl.json`),
+    samples(`${DOUGH_SAMPLES_BASE}/mridangam.json`),
+    samples(`${DIRT_SAMPLES_BASE}/strudel.json`),
   ]);
-  aliasBank(`${ts}/tidal-drum-machines-alias.json`);
+  await aliasBank(`${TODEPOND_SAMPLES_BASE}/tidal-drum-machines-alias.json`);
 }
 
 export { prebake };
