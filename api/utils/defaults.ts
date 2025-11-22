68926a55382bc3059f99c8e3e406235165847062
Nicholas Griffin <me@nicholasgriffin.co.uk>
2025-11-22 23:40:23 +0000

    fix respect setting

--- api/utils/defaults.ts
+++ api/utils/defaults.ts
@@ -53,7 +53,7 @@ export function getDefaultRoomSettings(
     autoHandoverModerator: settings?.autoHandoverModerator ?? false,
     enableStrudelPlayer: settings?.enableStrudelPlayer ?? true,
     strudelAutoGenerate: settings?.strudelAutoGenerate ?? false,
-    enableTicketQueue: true,
+    enableTicketQueue: settings?.enableTicketQueue ?? true,
     resultsDisplay: {
       showVoteDistribution: true,
       voteDistributionLabel: 'Vote Distribution',
