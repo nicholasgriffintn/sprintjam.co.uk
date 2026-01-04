6bfcd5eb48cce29fbe564a5f84db49db776f4316
Nicholas Griffin <me@nicholasgriffin.co.uk>
2026-01-04 19:31:27 +0000

    chore: remove old folder

--- tests/e2e/pageObjects/base-page.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from "@playwright/test";
-
-export class BasePage {
-  constructor(protected readonly page: Page) {}
-
-  async goto(path = "/") {
-    await this.page.goto(path);
-  }
-}
